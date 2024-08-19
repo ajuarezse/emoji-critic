8be26524c2fd5827e55f3980f0f1cec7b730c3a8
ajuarezse <ajuarez.se@gmail.com>
2024-08-19 13:13:04 -0700

    configure about me nest

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
-import "./App.css";
 import Header from "../Header/Header";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
 import AboutUs from "../AboutUs/AboutUs";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
+import "./App.css";
 
 function App() {
   return (
@@ -13,7 +16,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
         <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </div>
