88a78ee50bf1a29da8f7e59baa9d7e9d64bba603
ajuarezse <ajuarez.se@gmail.com>
2024-08-20 12:40:18 -0700

    add page not found

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,6 +12,7 @@ import AboutUs from "../AboutUs/AboutUs";
 import SiteHistory from "../AboutUs/SiteHistory";
 import SiteMission from "../AboutUs/SiteMission";
 import Review from "../Review/Review";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
   const [reviews, setReviews] = useState([]);
@@ -31,6 +32,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews reviews={reviews} />} />
         <Route
